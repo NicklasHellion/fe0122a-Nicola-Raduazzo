e6428634a8880748d9d6fe2e2689a52486f377b8
NicklasHellion <nicola.raduazzo@gmail.com>
2022-03-01 10:00:50 +0100

    Aggiunta s6 e g1 relativo

--- s5/progetto s5/script.js
+++ s5/progetto s5/script.js
@@ -2,6 +2,7 @@ const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
 const deleteAllBtn = document.querySelector(".footer button");
+
 inputBox.onkeyup = () => {
     let userEnteredValue = inputBox.value;
     if (userEnteredValue.trim() != 0) {
